3b26b73edf40120cfd751408eae007b29fa0327d
pilming44 <pilming44@naver.com>
2024-07-18 03:50:55 +0900

    사용하지않는 section레파지토리 삭제

--- src/main/java/nextstep/subway/repository/SectionRepository.java
+++
@@ -1,7 +0,0 @@
-package nextstep.subway.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import nextstep.subway.entity.Section;
-
-public interface SectionRepository extends JpaRepository<Section, Long> {
-}
