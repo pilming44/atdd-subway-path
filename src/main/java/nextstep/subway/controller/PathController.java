0f040ee1a541869b6d019d441029fb5598d5fee7
pilming44 <pilming44@naver.com>
2024-07-22 23:25:37 +0900

    컨트롤러에서 매개변수를 dto로 받게끔 수정

--- src/main/java/nextstep/subway/controller/PathController.java
+++ src/main/java/nextstep/subway/controller/PathController.java
@@ -1,12 +1,11 @@
 package nextstep.subway.controller;
 
+import nextstep.subway.dto.PathRequest;
 import nextstep.subway.dto.PathResponse;
-import nextstep.subway.entity.Station;
 import nextstep.subway.service.PathService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -19,7 +18,7 @@ public class PathController {
     }
 
     @GetMapping
-    public ResponseEntity<PathResponse> getPath(@RequestParam Station source, @RequestParam Station target) {
-        return ResponseEntity.ok().body(pathService.getPath(source, target));
+    public ResponseEntity<PathResponse> getPath(PathRequest pathRequest) {
+        return ResponseEntity.ok().body(pathService.getPath(pathRequest));
     }
 }
--- src/main/java/nextstep/subway/service/PathService.java
+++ src/main/java/nextstep/subway/service/PathService.java
@@ -1,10 +1,13 @@
 package nextstep.subway.service;
 
+import nextstep.subway.dto.PathRequest;
 import nextstep.subway.dto.PathResponse;
 import nextstep.subway.entity.Line;
 import nextstep.subway.entity.PathFinder;
 import nextstep.subway.entity.Station;
+import nextstep.subway.exception.NoSuchStationException;
 import nextstep.subway.repository.LineRepository;
+import nextstep.subway.repository.StationRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -14,15 +17,31 @@ import java.util.List;
 @Transactional(readOnly = true)
 public class PathService {
     private final LineRepository lineRepository;
+    private final StationRepository stationRepository;
 
-    public PathService(LineRepository lineRepository) {
+    public PathService(LineRepository lineRepository, StationRepository stationRepository) {
         this.lineRepository = lineRepository;
+        this.stationRepository = stationRepository;
     }
 
-    public PathResponse getPath(Station source, Station target) {
+    public PathResponse getPath(PathRequest pathRequest) {
+        Station sourceStation = null;
+        Station targetStation = null;
+
+        if (pathRequest.getSource() != null) {
+            sourceStation = getStation(pathRequest.getSource());
+        }
+        if (pathRequest.getTarget() != null) {
+            targetStation = getStation(pathRequest.getTarget());
+        }
         List<Line> allLines = lineRepository.findAll();
         PathFinder pathFinder = new PathFinder();
         allLines.stream().forEach(pathFinder::addLine);
-        return pathFinder.getPath(source, target);
+        return pathFinder.getPath(sourceStation, targetStation);
+    }
+
+    private Station getStation(Long stationId) {
+        return stationRepository.findById(stationId)
+                .orElseThrow(() -> new NoSuchStationException("존재하지 않는 역입니다."));
     }
 }
